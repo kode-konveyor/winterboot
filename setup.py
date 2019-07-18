55b8e23a5f5a0a57c69126174e12944fa6446767
mag <mag@kodekonveyor.com>
2019-07-18 09:35:04 +0000

    readme and setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.MD", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='winterboot',  
+     version='0.1',
+     author="Arpad Magosanyi",
+     author_email="mag@kodekonveyor.com",
+     description="Winterboot is not SpringBoot",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/kode-konveyor/winterboot",
+     package_dir = {'': 'src'},
+     packages=['winterboot'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+         "Operating System :: OS Independent",
+     ],
+
+ )
